05cd2f78b7d460b29a1e16c3b4498e6b4f1ca88a
Darren Matthews <darren.matthews@sorted.com>
2024-04-26 15:59:45 +0100

    Added variable fixed fmt

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "environment" {
 
 variable "billing_code" {
   type        = string
-  description = "(Required) Billing code for network resources"  
+  description = "(Required) Billing code for network resources"
 }
 
 variable "cidr_block" {
